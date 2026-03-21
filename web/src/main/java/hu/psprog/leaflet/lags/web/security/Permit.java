40d32bc8d8784865fbeb20ac3bfa460a30dc8670
Péter Kovács <peter.kovacs0519@gmail.com>
2026-03-21 23:22:35 +0100

    LFLT-650 Integrate improved role management  - Added explicit permission requirements for profile operations

--- web/src/main/java/hu/psprog/leaflet/lags/web/security/Permit.java
+++ web/src/main/java/hu/psprog/leaflet/lags/web/security/Permit.java
@@ -32,6 +32,13 @@ public interface Permit {
         @interface Permissions { }
 
         /**
+         * Allows reading own profile of the active user.
+         */
+        @Retention(RetentionPolicy.RUNTIME)
+        @PreAuthorize("hasAuthority('SCOPE_read:access:profile') || hasAuthority('read:access:profile')")
+        @interface Profile { }
+
+        /**
          * Allows reading roles.
          */
         @Retention(RetentionPolicy.RUNTIME)
@@ -64,6 +71,20 @@ public interface Permit {
         @Retention(RetentionPolicy.RUNTIME)
         @PreAuthorize("hasAuthority('SCOPE_write:access:permissions')")
         @interface Permissions { }
+
+        /**
+         * Allows modifying own profile of the active user.
+         */
+        @Retention(RetentionPolicy.RUNTIME)
+        @PreAuthorize("hasAuthority('SCOPE_write:access:profile') || hasAuthority('write:access:profile')")
+        @interface Profile { }
+
+        /**
+         * Allows deleting own profile of the active user.
+         */
+        @Retention(RetentionPolicy.RUNTIME)
+        @PreAuthorize("hasAuthority('SCOPE_write:access:profile:delete') || hasAuthority('write:access:profile:delete')")
+        @interface ProfileDelete { }
 
         /**
          * Allows writing (creating, editing and deleting) roles.
